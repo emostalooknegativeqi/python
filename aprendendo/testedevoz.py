a5920d748024101c58e3215f284aea2684ca6435
Cauã Santana <114744734+emostalooknegativeqi@users.noreply.github.com>
2023-05-13 00:10:37 -0300

    teste de voz e algumas outras coisinhas

--- aprendendo/testedevoz.py
+++ aprendendo/testedevoz.py
@@ -5,3 +5,9 @@ print("falando")
 engine = pyttsx3.init()
 engine.say(texto)
 engine.runAndWait()
+if texto.endswith("ão"):
+    engine.say("meu pau na sua mão kk")
+    engine.runAndWait()
+if texto.endswith("ido"):
+    engine.say("meu pau no seu ouvido kk")
+    engine.runAndWait()
